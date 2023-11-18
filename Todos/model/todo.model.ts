42af9ffcb3cb0eeb6df74b10771b92e43fddc3e3
[Luis de Haro Parames] <[luisdeharo4@gmail.com]>
2023-11-18 16:45:03 +0100

    Todo Crud

---
+++ Todos/model/todo.model.ts
@@ -0,0 +1,29 @@
+/**
+ * 
+ * 
+ * @Class Model
+ */
+
+export interface todo {
+    text: string,
+    complete?: boolean
+}
+
+export class ModelTodo {
+    public id: string;
+    public text: string;
+    public complete?: boolean;
+
+    constructor({ text, complete }: todo = {
+        text: '',
+        complete: false
+    }) {
+        this.id = this.generateId();
+        this.text = text;
+        this.complete = complete;
+    }
+
+    generateId(): string {
+        return Math.random().toString();
+    }
+}
